10fe3662c70f4e774166f2745674a6c9d6d9351a
Diego2250 <mor21146@uvg.edu.gt>
2022-02-18 23:08:37 -0600

    update y eliminacion de clase archivo.java
    Se ha implementado el leer el archivo en el main

--- main.java
+++ main.java
@@ -1,26 +1,32 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 /**
  * @author Diego Morales
- *
  */
 public class main {
 	
-static String ln;
-	/***
-	 * Funcion del programa principal
-	 * @param args si es necesario agregar argumentos al ejecutar 
-	 * @throws Exception
-	 */
 	public static void main(String[] args)throws Exception{
 		CalculadoraPosfix calc = new CalculadoraPosfix();
-		//File file = new File("datos.txt");
-        //Scanner s = new Scanner(file);
-		//while(s.hasNextLine()){
-		 //ln = s.nextLine();
-		 //}
-		calc.Evaluate("6 2 3 + * ");
-		System.out.println(calc.resultado);
-
+		try {
+            File file = new File("datos.txt");
+            Scanner s = new Scanner(file);
+            while(s.hasNextLine()){
+            String data = s.nextLine();
+            System.out.println("Se ha leido del archivo: ");
+            System.out.println(data); 
+            try {
+                calc.Evaluate(data);
+                System.out.println("El resultado de esta operación es: ");
+                System.out.println(calc.resultado);
+            } catch (Exception e) {
+                System.out.println("Ha ocurrido un error, por favor, intentalo de nuevo con una expresión diferente"); 
+            }
+    
+            }
+            s.close(); 
+        } catch(FileNotFoundException e){
+            e.printStackTrace(); 
+        }
 	  }
 }
